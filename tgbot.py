1d160b53bf0cf23a026c53236597e60270b7ae47
leksus <leksus@leksus.info>
2022-07-10 17:34:46 +0300

    working bot with pdf files

--- tgbot.py
+++ tgbot.py
@@ -1,33 +1,80 @@
 import logging
 import os
 
-from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import FSInputFile
+from dotenv import load_dotenv
+from libmat2 import parser_factory
 
-logging.basicConfig(level=logging.INFO)
 
-load_dotenv()
+
+UPLOAD_FOLDER = 'upload/'
+ALLOWED_EXTENSIONS = {'pdf'}
 
 dp = Dispatcher()
-bot = Bot(os.getenv('TOKEN'), parse_mode="HTML")
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@dp.message(commands=["start"])
+async def command_start_handler(message: types.message):
+    await message.answer(f'''
+Привет! Я чищу метадату в файлах. Поддерживаются следующие форматы:
+{', '.join(ALLOWED_EXTENSIONS)}
+Папки и архивы пока не работают, но можно кидать сразу несколько файлов! 
+        '''
+    )
+
 
 @dp.message()
-async def scan_message(message: types.Message):
+async def prepare_message(message: types.document):
 
     try:
         file_obj = message.document
+        file = await bot.get_file(file_obj.file_id)
+
     except Exception as e:
-        await message.answer('Error while downloading file')
+        await message.answer('Пожалуйста, пришлите файл')
+        logger.info("User send not a file")
         return
 
-    file = await bot.get_file(file_obj.file_id)
+    if not allowed_file(file_obj.file_name):
+        await message.answer('Данный тип файлов не поддерживается. Поддерживаются: ' + ', '.join(ALLOWED_EXTENSIONS))
+        logger.info("User send wrong file format")
+        return
 
-    await bot.download_file(file.file_path, file_obj.file_name)
 
-    agenda = FSInputFile(file_obj.file_name, filename=file_obj.file_name)
+    local_filepath = UPLOAD_FOLDER + file_obj.file_name
+    try:
+        await bot.download_file(file.file_path, local_filepath)
+    except Exception:
+        await message.answer('Проблема с сохранением файла на сервере, обратитесь к разработчику')
+        logger.error("Can't save file on server. Permissions, wrong path, UPLOAD_FOLDER does not exist?")
+        return
 
-    await message.answer_document(agenda)
+    try:
+        parser, mime = parser_factory.get_parser(local_filepath)
+
+        parser.lightweight_cleaning = True
+        parser.remove_all()
+        os.rename(parser.output_filename, local_filepath)
+        output_file = FSInputFile(local_filepath)
+        await message.answer_document(output_file)
+
+    except ValueError:
+        await message.answer('Ошибка чистки метаданных. Неверный формат файла?')
+        logger.warning("Can't clean metadata. Wrong file format or something with mat2")
+        return
+
 
 if __name__ == "__main__":
+    load_dotenv()
+    bot = Bot(os.getenv('TOKEN'))
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    )
+    logger = logging.getLogger(__name__)
+    logger.info("Starting bot")
     dp.run_polling(bot)
